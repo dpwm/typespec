c4cca5b9ddab6b85fbfd211d0ddf00e519668fd7
David Mallows <dave.mallows@gmail.com>
2018-02-07 18:43:47 +0000

    Workable GADT prototype

--- src/typespec.ml
+++ src/typespec.ml
@@ -1,3 +1,40 @@
+module Converter = struct
+  type ('a, 'b) t = {
+    get: 'a -> 'b;
+    set: 'b -> 'a;
+  };;
+end;;
+
+module Record = struct
+  type ('a, 'b, 'composite) t = {
+    composite : 'composite;
+    converter : ('a, 'b) Converter.t;
+    fieldnames : string list;
+  }
+end;;
+
+module Tuple = struct
+  type ('a, 'b, 'composite) t = {
+    composite : 'composite;
+    converter : ('a, 'b) Converter.t;
+  }
+end;;
+
+type 'a t =
+  | Unit  : unit t
+  | Int   : int t
+  | Float : float t
+  | String : string t
+  | Bool  : bool t
+
+  | List : 'a t -> 'a list t
+  | Array : 'a t -> 'a array t
+
+  | Record : ('a, 'b, 'b composite) Record.t -> 'a t
+  | Tuple : ('a, 'b, 'b composite) Tuple.t -> 'a t
 
 
-let () = Js.log "Hello, BuckleScript"
+and 'a composite =
+  | Field: ('b t * 'c composite) -> ('b * 'c) composite
+  | End : unit composite
+
