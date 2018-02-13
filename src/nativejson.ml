f72d484cc3af4e3b93b44e9669c1dc421ff6429a
David Mallows <dave.mallows@gmail.com>
2018-02-13 11:11:40 +0000

    Adding native (yojson) serializer

---
+++ src/nativejson.ml
@@ -0,0 +1,23 @@
+open Typespec
+
+module JsonSerializer : Serializer with type st = Yojson.Basic.json = struct
+  type st = Yojson.Basic.json
+
+  let of_float x = `Float x
+  let of_int x = `Int x
+  let of_bool x = `Int (if x then 1 else 0)
+  let of_string x = `String x
+
+  let of_list f x = `List (List.map f x)
+  let of_array f x = x |> Array.to_list |> of_list f
+
+  let of_tuple cser x =
+    `List (CompositeSerializer.apply cser x)
+
+  let of_record _ = of_tuple
+end;;
+
+module Serializer = Typespec.MakeSerializer(JsonSerializer)
+
+let serialize = Serializer.serialize
+
